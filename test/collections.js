0c3a96b0409201158990b3734db9193d5d4282da
Francis Asante <kofrasa@gmail.com>
2013-10-27 01:15:12 +0000

    added tests for $group operators

--- test/collections.js
+++ test/collections.js
@@ -36,14 +36,14 @@ isReady.then(function () {
     ok(cursor.count() < 800, "can query Backbone collection");
   });
 
-  test("Aggregation", function () {
+  test("pipeline operators", function () {
     var students = testData['students'];
 
     var flattened = Mingo.aggregate(students, {'$unwind': '$scores'});
-    equal(flattened.length, 800, "can flatten collection with $unwind");
+    ok(flattened.length === 800, "can flatten collection with $unwind");
 
     var result = Mingo.aggregate(flattened, {'$match': {_id: 0}});
-    equal(result.length, 4, "can filter collection with $match");
+    ok(result.length === 4, "can filter collection with $match");
 
     // find highest and lowest scores for each type
     var grouped = Mingo.aggregate(
@@ -51,14 +51,17 @@ isReady.then(function () {
       {'$group': {'_id': '$scores.type', 'highest': {$max: '$scores.score'},
         'lowest': {$min: '$scores.score'}, 'average': {$avg: '$scores.score'}, 'count': {$sum: 1}}}
     );
-    equal(grouped.length, 3, "can group collection with $group");
+    ok(grouped.length === 3, "can group collection with $group");
 
+  });
 
+  test("$group operators", function () {
     // generate some sample data
     var sample = [];
     for (var i = 0; i < 10; i++) {
       sample.push({
         '_id': i,
+        'class': ['A','B'][_.random(1)],
         'score': i + 10
       })
     }
@@ -66,15 +69,22 @@ isReady.then(function () {
     result = Mingo.aggregate(
       sample,
       {'$group': {'_id': 'stats', 'max': {$max: '$score'},
-        'min': {$min: '$score'}, 'avg': {$avg: '$score'}, 'count': {$sum: 1}}}
+        'min': {$min: '$score'}, 'avg': {$avg: '$score'}, 'count': {$sum: 1},
+        'classes': {$push: '$class'}, 'set': {$addToSet: '$class'},
+      }}
     );
 
     // average := 14.5, max := 19, min: 10, count: 10
     equal(result[0]['max'], 19, "can apply $max in grouping");
     equal(result[0]['min'], 10, "can apply $min in grouping");
     equal(result[0]['avg'], 14.5, "can apply $avg in grouping");
-    equal(result[0]['count'], 10, "can apply $count in grouping");
+    equal(result[0]['count'], 10, "can apply $sum in grouping");
 
+    var val = result[0]['classes'];
+    ok(val.length == 10 && (_.contains(val, 'A') || _.contains(val, 'B')), "can apply $push in grouping");
+
+    val = result[0]['set'];
+    ok(val.length === 2 && (_.contains(val, 'A') && _.contains(val, 'B')), "can apply $addToSet in grouping");
   });
 
 });
