f7ef2440f461eae24dce308499e618b94d4ebeba
Dan Levy <dan@danlevy.net>
2016-09-17 23:45:49 -0600

    cleaned up webpack script

--- webpack.config.js
+++ webpack.config.js
@@ -34,31 +34,12 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel', // 'babel-loader' is also a legal name to reference
+        loader: 'babel',
         query: {
           plugins: [
-            'transform-es2015-template-literals',
-            'transform-es2015-literals',
-            'transform-es2015-function-name',
-            'transform-es2015-arrow-functions',
-            'transform-es2015-block-scoped-functions',
-            'transform-es2015-classes',
-            'transform-es2015-object-super',
-            'transform-es2015-shorthand-properties',
-            'transform-es2015-computed-properties',
-            'transform-es2015-for-of',
-            'transform-es2015-sticky-regex',
-            'transform-es2015-unicode-regex',
-            'check-es2015-constants',
-            'transform-es2015-spread',
-            'transform-es2015-parameters',
-            'transform-es2015-destructuring',
-            'transform-es2015-block-scoping',
-            'transform-es2015-typeof-symbol',
-            ['transform-regenerator', { async: false, asyncGenerators: false }],
             'lodash'
           ],
-          // presets: ['es2015']
+          presets: ['es2015']
         }
       }
     ]
