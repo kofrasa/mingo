36f51e22002efcd344aca429e35e367f19492c35
Kevin Sloan <sloankev@gmail.com>
2017-02-04 18:02:49 -0500

    fix "$ifNull" assertion, tests for "$ifNull" (#45)
    * fix "$ifNull" assertion, tests for "$ifNull"
    * add $switch to README.md

--- test/aggregation.js
+++ test/aggregation.js
@@ -894,7 +894,7 @@ test("Boolean Operators", function (t) {
 });
 
 test("Conditional Operators", function (t) {
-  t.plan(2);
+  t.plan(4);
 
   var products = [
     {"_id": 1, "item": "abc1", description: "product 1", qty: 400},
@@ -947,6 +947,36 @@ test("Conditional Operators", function (t) {
     {"_id": 4, "item": "VWZ1", "stock": "normal"},
     {"_id": 5, "item": "VWZ2", "stock": "low"}
   ], result, "can apply $switch aggregate operator");
+
+  result = Mingo.aggregate([
+    {"_id" : 1, "item" : "abc1", description: "product 1", qty: 300},
+    {"_id" : 2, "item" : "abc2", description: null, qty: 200},
+    {"_id" : 3, "item" : "xyz1", qty: 250}
+  ], [{
+    $project: {
+      item: 1,
+      description: {$ifNull: [ "$description", "Unspecified" ]}
+    }
+  }]);
+
+  t.deepEqual([
+    {"_id" : 1, "item" : "abc1", "description" : "product 1"},
+    {"_id" : 2, "item" : "abc2", "description" : "Unspecified"},
+    {"_id" : 3, "item" : "xyz1", "description" : "Unspecified"}
+  ], result, "can apply $ifNull aggregate operator");
+
+  // expect $ifNull to throw if num of args are wrong
+  t.throws( function () {
+    Mingo.aggregate([
+      {"_id" : 1, "item" : "abc1", description: "product 1", qty: 300},
+    ], [{
+      $project: {
+        item: 1,
+        description: {$ifNull: [ "$description", "Unspecified", "" ]}
+      }
+    }])
+  }, /Invalid arguments for \$ifNull operator/, 
+  '$ifNull throws with wrong arguments')
 
   t.end();
 
