22f85d1dedbada3c2243e18c6698acee1cd9fa9d
Kevin Sloan <sloankev@gmail.com>
2017-02-04 03:48:00 -0500

    Switch operator (#44)
    * allow use of "$in" and "$nin" as aggregation comparison operators
    * implement "$switch" operator, tests for "$switch" and "$cond"

--- test/aggregation.js
+++ test/aggregation.js
@@ -643,6 +643,65 @@ test("Boolean Operators", function (t) {
   t.end();
 });
 
+test("Conditional Operators", function (t) {
+  t.plan(2);
+
+  var products = [
+    {"_id": 1, "item": "abc1", description: "product 1", qty: 400},
+    {"_id": 2, "item": "abc2", description: "product 2", qty: 200},
+    {"_id": 3, "item": "xyz1", description: "product 3", qty: 150},
+    {"_id": 4, "item": "VWZ1", description: "product 4", qty: 300},
+    {"_id": 5, "item": "VWZ2", description: "product 5", qty: 80}
+  ];
+
+  var result = Mingo.aggregate(products, [{
+    $project: {
+      item: 1,
+      stock: { 
+        $cond: {
+          if: {$lte: ["$qty", 200]}, 
+          then: "low", 
+          else: "high"
+        }
+      }
+    }
+  }]);
+
+  t.deepEqual([
+    {"_id": 1, "item": "abc1", "stock": "high"},
+    {"_id": 2, "item": "abc2", "stock": "low"},
+    {"_id": 3, "item": "xyz1", "stock": "low"},
+    {"_id": 4, "item": "VWZ1", "stock": "high"},
+    {"_id": 5, "item": "VWZ2", "stock": "low"}
+  ], result, "can apply $cond aggregate operator");
+
+  result = Mingo.aggregate(products, [{
+    $project: {
+      item: 1,
+      stock: {
+        $switch: {
+          branches: [
+            {case: {$lte: ["$qty", 200]}, then: "low"},
+            {case: {$gte: ["$qty", 400]}, then: "high"},
+          ],
+          default: "normal"
+        }
+      }
+    }
+  }]);
+
+  t.deepEqual([
+    {"_id": 1, "item": "abc1", "stock": "high"},
+    {"_id": 2, "item": "abc2", "stock": "low"},
+    {"_id": 3, "item": "xyz1", "stock": "low"},
+    {"_id": 4, "item": "VWZ1", "stock": "normal"},
+    {"_id": 5, "item": "VWZ2", "stock": "low"}
+  ], result, "can apply $switch aggregate operator");
+
+  t.end();
+
+})
+
 test("Variable Operators", function (t) {
   t.plan(2);
   var result = Mingo.aggregate([
