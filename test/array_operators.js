b5c5b2168d950b9cdeb8fd6777ece34b45c3c3c7
Francis Asante <kofrasa@gmail.com>
2017-02-08 12:21:13 +0000

    Added test for $in array operator

--- test/array_operators.js
+++ test/array_operators.js
@@ -501,5 +501,29 @@ test("Array Operators", function (t) {
 
   tryExamples(examples, "$reduce");
 
+  // $in
+  data = [
+    { "_id" : 1, "location" : "24th Street", "in_stock" : [ "apples", "oranges", "bananas" ] },
+    { "_id" : 2, "location" : "36th Street", "in_stock" : [ "bananas", "pears", "grapes" ] },
+    { "_id" : 3, "location" : "82nd Street", "in_stock" : [ "cantaloupes", "watermelons", "apples" ] }
+  ];
+
+  result = Mingo.aggregate(data, [
+    {
+      $project: {
+        "store location" : "$location",
+        "has bananas" : {
+          $in: [ "bananas", "$in_stock" ]
+        }
+      }
+    }
+  ]);
+
+  t.deepEqual(result, [
+    { "_id" : 1, "store location" : "24th Street", "has bananas" : true },
+    { "_id" : 2, "store location" : "36th Street", "has bananas" : true },
+    { "_id" : 3, "store location" : "82nd Street", "has bananas" : false }
+  ], "can apply $in array aggregation operator");
+
   t.end();
 });
