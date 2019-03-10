43705e4137e5112a8469f2ccd81ebfc295e67601
Francis Asante <kofrasa@gmail.com>
2019-03-10 15:23:22 +0200

    Add collation support to sort function. Fixes #100
    Support collation option in https://docs.mongodb.com/manual/reference/collation/.
    This allows more flexible options when using the sort function on the cursor.
    Some of the options of the collation spec have not been implemented because they do not map well to `localeCompare`.
    These include; maxVariable, backwards, and normalization.

--- test/pipeline/sort_operator.js
+++ test/pipeline/sort_operator.js
@@ -4,7 +4,6 @@ var samples = require('../support')
 var _ = mingo._internal()
 
 test("$sort pipeline operator", function (t) {
-  t.plan(2);
   var result = mingo.aggregate(samples.studentsData, [
     { '$sort': { '_id': -1 } }
   ]);
@@ -23,4 +22,124 @@ test("$sort pipeline operator", function (t) {
 
   result = mingo.aggregate(data, [{ "$sort": { "date": 1 } }]);
   t.deepEqual(result, expected, "can sort on complex fields");
+  t.end()
 });
+
+test('sort with collation', function (t) {
+
+  // english
+  let english = [
+    { "name" : "Bob" },
+    { "name" : "Tom" },
+    { "name" : "alice" },
+    { "name" : "peggy" },
+    { "name" : "21" },
+    { "name" : "100" },
+  ]
+  result = mingo.find(english).collation({locale: "en"}).sort({name: 1}).all()
+  t.deepEqual(result, [
+    { "name" : "100" },
+    { "name" : "21" },
+    { "name" : "alice" },
+    { "name" : "Bob" },
+    { "name" : "peggy" },
+    { "name" : "Tom" },
+  ], 'can sort with locale')
+
+  result = mingo.find(english).collation({locale: "en", numericOrdering: true}).sort({name: 1}).all()
+  t.deepEqual(result, [
+    { "name" : "21" },
+    { "name" : "100" },
+    { "name" : "alice" },
+    { "name" : "Bob" },
+    { "name" : "peggy" },
+    { "name" : "Tom" }
+  ], 'can sort with numeric odering')
+
+  // french
+  let french = [
+    {"name": "a"},
+    {"name": "B"},
+    {"name": "b"},
+    {"name": "c"},
+    {"name": "á"},
+    {"name": "A"}
+  ]
+  result = mingo.find(french).collation({locale: "fr"}).sort({name: 1}).all()
+  t.deepEqual(result, [
+    { name: 'a' },
+    { name: 'A' },
+    { name: 'á' },
+    { name: 'b' },
+    { name: 'B' },
+    { name: 'c' }
+  ], 'can sort with accented letters')
+
+  // upper case letters should come before lower case letters
+  result = mingo.find(french).collation({locale: "fr", caseFirst: 'upper'}).sort({name: 1}).all()
+  t.deepEqual(result, [
+    { name: 'A' },
+    { name: 'a' },
+    { name: 'á' },
+    { name: 'B' },
+    { name: 'b' },
+    { name: 'c' }
+  ], 'can sort upper case letters before lower case')
+
+  // 1:base compare strength
+  result = mingo.find(french).collation({locale: "fr", strength: 1}).sort({name: 1}).all()
+  t.deepEqual(result, [
+    {"name": "a"},
+    {"name": "á"},
+    {"name": "A"},
+    {"name": "B"},
+    {"name": "b"},
+    {"name": "c"},
+  ], 'sort should consider only base differences')
+
+  // 2:accent compare strength
+  result = mingo.find(french).collation({locale: "fr", strength: 2}).sort({name: 1}).all()
+  t.deepEqual(result, [
+    {"name": "a"},
+    {"name": "A"},
+    {"name": "á"},
+    {"name": "B"},
+    {"name": "b"},
+    {"name": "c"},
+  ], 'can sort accented letters')
+
+  // spanish
+  let spanish = [
+    { "name": "Ánfora" },
+    { "name": "Óscar" },
+    { "name": "Barça" },
+    { "name": "Niño" },
+    { "name": "¡Hola!"},
+    { "name": "¿qué?" }
+  ]
+  result = mingo.find(spanish).collation({locale: 'es'}).sort({name: -1}).all()
+  t.deepEqual(result, [
+    { name: 'Óscar' },
+    { name: 'Niño' },
+    { name: 'Barça' },
+    { name: 'Ánfora' },
+    { name: '¿qué?' },
+    { name: '¡Hola!' }
+  ], 'can sort letters reversed')
+
+  // ignore punctuations. this should preserve the same order as the input
+  result = mingo.find([
+    {name: 'Hello there'},
+    {name: 'Hello,there'},
+    {name: 'Hello  there'},
+    {name: 'Hello,there'},
+  ]).collation({locale: 'en', alternate: 'shifted'}).sort().all()
+  t.deepEqual(result, [
+    {name: 'Hello there'},
+    {name: 'Hello,there'},
+    {name: 'Hello  there'},
+    {name: 'Hello,there'},
+  ], 'can sort letters ignoring punctuations')
+
+  t.end()
+})
