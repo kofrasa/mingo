270181b97976381bc944b057a823430e85f099f7
Sergii <sergiy.stotskiy@gmail.com>
2019-07-08 20:45:12 +0300

    chore(build): replaces Makefile + gulp with npm scripts

--- config/rollup.es.js
+++ config/rollup.es.js
@@ -1,20 +1,19 @@
 import fs from 'fs'
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
+import packageDetails from '../package.json'
 
 
 const MODULE = 'mingo'
-const VERSION = fs.readFileSync(__dirname +'/../VERSION').toString().trim()
-const BANNER = fs.readFileSync(__dirname + '/../templates/header.txt').toString()
+const BANNER = fs.readFileSync(`${__dirname}/../templates/header.txt`, 'utf8')
   .replace('@YEAR', new Date().getFullYear())
-  .replace('@VERSION', VERSION)
-
+  .replace('@VERSION', packageDetails.version)
 
 function version () {
   return {
     name: 'version',
     transformBundle (code) {
-      return code.replace(/VERSION\s+=\s+(['"])[\d\.]+\1/, `VERSION = '${VERSION}'`)
+      return code.replace(/VERSION\s+=\s+(['"])[\d\.]+\1/, `VERSION = '${packageDetails.version}'`)
     }
   }
 }
