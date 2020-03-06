f26451aaa981fb46c27e10ddde911eebc09e0985
Francis Asante <kofrasa@gmail.com>
2020-03-06 15:26:47 +0200

    Projection operator enhancement and fixes
    - Support new array fields. Fixes #119
    - Handle embedded document exclusions

--- dist/mingo.js
+++ dist/mingo.js
@@ -935,7 +935,7 @@ function truncate(num, places, roundOff) {
       while (excess > 10) {
         excess -= excess % 10;
       }
-      if (roundOff && result > 0 && excess >= 5) {
+      if (result > 0 && excess >= 5) {
         result += _offset;
       }
     }
@@ -2085,122 +2085,155 @@ function $project(collection, expr, opt) {
   if (isEmpty(expr)) return collection;
 
   // result collection
-  var objKeys = keys(expr);
+  var expressionKeys = keys(expr);
   var idOnlyExcludedExpression = false;
   var ID_KEY = idKey();
 
   // validate inclusion and exclusion
+  validateExpression(expr);
+
+  if (inArray(expressionKeys, ID_KEY)) {
+    var id = expr[ID_KEY];
+    if (id === 0 || id === false) {
+      expressionKeys = expressionKeys.filter(notInArray.bind(null, [ID_KEY]));
+      assert(notInArray(expressionKeys, ID_KEY), 'Must not contain collections id key');
+      idOnlyExcludedExpression = isEmpty(expressionKeys);
+    }
+  } else {
+    // if not specified the add the ID field
+    expressionKeys.push(ID_KEY);
+  }
+
+  return collection.map(function (obj) {
+    return processObject(obj, expr, expressionKeys, idOnlyExcludedExpression);
+  });
+}
+
+function processObject(obj, expr, expressionKeys, idOnlyExcludedExpression) {
+  var ID_KEY = idKey();
+
+  var newObj = {};
+  var foundSlice = false;
+  var foundExclusion = false;
+  var dropKeys = [];
+
+  if (idOnlyExcludedExpression) {
+    dropKeys.push(ID_KEY);
+  }
+
+  expressionKeys.forEach(function (key) {
+    // final computed value of the key
+    var value = void 0;
+
+    // expression to associate with key
+    var subExpr = expr[key];
+
+    if (key !== ID_KEY && inArray([0, false], subExpr)) {
+      foundExclusion = true;
+    }
+
+    if (key === ID_KEY && isEmpty(subExpr)) {
+      // tiny optimization here to skip over id
+      value = obj[key];
+    } else if (isString(subExpr)) {
+      value = computeValue(obj, subExpr, key);
+    } else if (inArray([1, true], subExpr)) {
+      // For direct projections, we use the resolved object value
+    } else if (isArray(subExpr)) {
+      value = subExpr.map(function (v) {
+        var r = computeValue(obj, v);
+        if (isNil(r)) return null;
+        return r;
+      });
+    } else if (isObject(subExpr)) {
+      var subExprKeys = keys(subExpr);
+      var operator = subExprKeys.length > 1 ? false : subExprKeys[0];
+
+      if (inArray(ops(OP_PROJECTION), operator)) {
+        // apply the projection operator on the operator expression for the key
+        if (operator === '$slice') {
+          // $slice is handled differently for aggregation and projection operations
+          if (ensureArray(subExpr[operator]).every(isNumber)) {
+            // $slice for projection operation
+            value = projectionOperators[operator](obj, subExpr[operator], key);
+            foundSlice = true;
+          } else {
+            // $slice for aggregation operation
+            value = computeValue(obj, subExpr, key);
+          }
+        } else {
+          value = projectionOperators[operator](obj, subExpr[operator], key);
+        }
+      } else {
+        // compute the value for the sub expression for the key
+        if (has(obj, key)) {
+          validateExpression(subExpr);
+          var nestedObj = obj[key];
+          value = isArray(nestedObj) ? nestedObj.map(function (o) {
+            return processObject(o, subExpr, subExprKeys, false);
+          }) : processObject(nestedObj, subExpr, subExprKeys, false);
+        } else {
+          value = computeValue(obj, subExpr, key);
+        }
+      }
+    } else {
+      dropKeys.push(key);
+      return;
+    }
+
+    // get value with object graph
+    var objPathValue = resolveObj(obj, key, {
+      preserveMissingValues: true
+    });
+
+    // add the value at the path
+    if (objPathValue !== undefined) {
+      merge(newObj, objPathValue, {
+        flatten: true
+      });
+    }
+
+    // if computed add/or remove accordingly
+    if (notInArray([0, 1, false, true], subExpr)) {
+      if (value === undefined) {
+        removeValue(newObj, key);
+      } else {
+        setValue(newObj, key, value);
+      }
+    }
+  });
+
+  // filter out all missing values preserved to support correct merging
+  filterMissing(newObj);
+
+  // if projection included $slice operator
+  // Also if exclusion fields are found or we want to exclude only the id field
+  // include keys that were not explicitly excluded
+  if (foundSlice || foundExclusion || idOnlyExcludedExpression) {
+    newObj = Object.assign({}, obj, newObj);
+    if (dropKeys.length > 0) {
+      newObj = cloneDeep(newObj);
+      each(dropKeys, function (k) {
+        return removeValue(newObj, k);
+      });
+    }
+  }
+
+  return newObj;
+}
+
+// validate inclusion and exclusion
+function validateExpression(expr) {
+  var ID_KEY = idKey();
   var check = [false, false];
   each(expr, function (v, k) {
     if (k === ID_KEY) return;
     if (v === 0 || v === false) {
       check[0] = true;
-    } else {
+    } else if (v === 1 || v === true) {
       check[1] = true;
     }
-    assert(check[0] !== check[1], 'Projection cannot have a mix of inclusion and exclusion.');
-  });
-
-  if (inArray(objKeys, ID_KEY)) {
-    var id = expr[ID_KEY];
-    if (id === 0 || id === false) {
-      objKeys = objKeys.filter(notInArray.bind(null, [ID_KEY]));
-      assert(notInArray(objKeys, ID_KEY), 'Must not contain collections id key');
-      idOnlyExcludedExpression = isEmpty(objKeys);
-    }
-  } else {
-    // if not specified the add the ID field
-    objKeys.push(ID_KEY);
-  }
-
-  return collection.map(function (obj) {
-    var newObj = {};
-    var foundSlice = false;
-    var foundExclusion = false;
-    var dropKeys = [];
-
-    if (idOnlyExcludedExpression) {
-      dropKeys.push(ID_KEY);
-    }
-
-    each(objKeys, function (key) {
-      var subExpr = expr[key];
-      var value = void 0; // final computed value of the key
-
-      if (key !== ID_KEY && inArray([0, false], subExpr)) {
-        foundExclusion = true;
-      }
-
-      if (key === ID_KEY && isEmpty(subExpr)) {
-        // tiny optimization here to skip over id
-        value = obj[key];
-      } else if (isString(subExpr)) {
-        value = computeValue(obj, subExpr, key);
-      } else if (inArray([1, true], subExpr)) {
-        // For direct projections, we use the resolved object value
-      } else if (isObject(subExpr)) {
-        var operator = keys(subExpr);
-        operator = operator.length > 1 ? false : operator[0];
-
-        if (inArray(ops(OP_PROJECTION), operator)) {
-          // apply the projection operator on the operator expression for the key
-          if (operator === '$slice') {
-            // $slice is handled differently for aggregation and projection operations
-            if (ensureArray(subExpr[operator]).every(isNumber)) {
-              // $slice for projection operation
-              value = projectionOperators[operator](obj, subExpr[operator], key);
-              foundSlice = true;
-            } else {
-              // $slice for aggregation operation
-              value = computeValue(obj, subExpr, key);
-            }
-          } else {
-            value = projectionOperators[operator](obj, subExpr[operator], key);
-          }
-        } else {
-          // compute the value for the sub expression for the key
-          value = computeValue(obj, subExpr, key);
-        }
-      } else {
-        dropKeys.push(key);
-        return;
-      }
-
-      // get value with object graph
-      var objPathValue = resolveObj(obj, key, { preserveMissingValues: true });
-
-      // add the value at the path
-      if (objPathValue !== undefined) {
-        merge(newObj, objPathValue, { flatten: true });
-      }
-
-      // if computed add/or remove accordingly
-      if (notInArray([0, 1, false, true], subExpr)) {
-        if (value === undefined) {
-          removeValue(newObj, key);
-        } else {
-          setValue(newObj, key, value);
-        }
-      }
-    });
-
-    // filter out all missing values preserved to support correct merging
-    filterMissing(newObj);
-
-    // if projection included $slice operator
-    // Also if exclusion fields are found or we want to exclude only the id field
-    // include keys that were not explicitly excluded
-    if (foundSlice || foundExclusion || idOnlyExcludedExpression) {
-      newObj = Object.assign({}, obj, newObj);
-      if (dropKeys.length > 0) {
-        newObj = cloneDeep(newObj);
-        each(dropKeys, function (k) {
-          return removeValue(newObj, k);
-        });
-      }
-    }
-
-    return newObj;
+    assert(!(check[0] && check[1]), 'Projection cannot have a mix of inclusion and exclusion.');
   });
 }
 
--- test/pipeline/project_operator.js
+++ test/pipeline/project_operator.js
@@ -110,11 +110,11 @@ samples.runTestPipeline("$project pipeline operator", [
       { '$limit': 1 }
     ],
     check: function (result, t) {
-    var fields = Object.keys(result[0]);
-    t.ok(fields.length === 2, "2/3 fields are included. Instead: " + fields.length);
-    t.ok(fields.indexOf('name') >= 0, "name is included");
-    t.ok(fields.indexOf('_id') === -1, "_id is excluded");
-    t.ok(fields.indexOf('scores') >= 0, "score is included");
+      var fields = Object.keys(result[0]);
+      t.ok(fields.length === 2, "2/3 fields are included. Instead: " + fields.length);
+      t.ok(fields.indexOf('name') >= 0, "name is included");
+      t.ok(fields.indexOf('_id') === -1, "_id is excluded");
+      t.ok(fields.indexOf('scores') >= 0, "score is included");
     }
   },
 
@@ -141,10 +141,62 @@ samples.runTestPipeline("$project pipeline operator", [
     message: 'can $project new field with group operator'
   },
 
+  {
+    messge: "exclude fields from embedded documents",
+    input: [
+      {
+        "_id" : 1,
+        title: "abc123",
+        isbn: "0001122223334",
+        author: { last: "zzz", first: "aaa" },
+        copies: 5,
+        lastModified: "2016-07-28"
+      }
+    ],
+    query: [ { $project : { "author.first" : 0, "lastModified" : 0 } } ],
+    check: [
+      {
+        "_id" : 1,
+        "title" : "abc123",
+        "isbn" : "0001122223334",
+        "author" : {
+           "last" : "zzz"
+        },
+        "copies" : 5,
+      }
+    ]
+  },
+
+  {
+    messge: "exclude fields from embedded documents using nested array syntax",
+    input: [
+      {
+        "_id" : 1,
+        title: "abc123",
+        isbn: "0001122223334",
+        author: { last: "zzz", first: "aaa" },
+        copies: 5,
+        lastModified: "2016-07-28"
+      }
+    ],
+    query: [ { $project: { "author": { "first": 0 }, "lastModified" : 0 } } ],
+    check: [
+      {
+        "_id" : 1,
+        "title" : "abc123",
+        "isbn" : "0001122223334",
+        "author" : {
+          "last" : "zzz"
+        },
+        "copies" : 5,
+      }
+    ]
+  },
+
   // Project with $$REMOVE
   // See: https://docs.mongodb.com/manual/reference/operator/aggregation/project/#remove-example
   {
-    message: '$project conditionally exclude fields with $$REMOVE ',
+    message: '$project conditionally exclude fields',
     input: [
       {
         "_id" : 1,
@@ -173,18 +225,18 @@ samples.runTestPipeline("$project pipeline operator", [
     ],
     query: [
       {
-         $project: {
-            title: 1,
-            "author.first": 1,
-            "author.last" : 1,
-            "author.middle": {
-               $cond: {
-                  if: { $eq: [ "", "$author.middle" ] },
-                  then: "$$REMOVE",
-                  else: "$author.middle"
-               }
-            }
-         }
+        $project: {
+          title: 1,
+          "author.first": 1,
+          "author.last" : 1,
+          "author.middle": {
+              $cond: {
+                if: { $eq: [ "", "$author.middle" ] },
+                then: "$$REMOVE",
+                else: "$author.middle"
+              }
+          }
+        }
       }
     ],
     check: [
@@ -192,6 +244,103 @@ samples.runTestPipeline("$project pipeline operator", [
       { "_id" : 2, "title" : "Baked Goods", "author" : { "last" : "xyz", "first" : "abc" } },
       { "_id" : 3, "title" : "Ice Cream Cakes", "author" : { "last" : "xyz", "first" : "abc", "middle" : "mmm" } }
     ]
+  },
+  {
+    message: "project include specific fields from embedded documents",
+    input: [
+      { _id: 1, user: "1234", stop: { title: "book1", author: "xyz", page: 32 } },
+      { _id: 2, user: "7890", stop: [ { title: "book2", author: "abc", page: 5 }, { title: "book3", author: "ijk", page: 100 } ] }
+    ],
+    query: [
+      { $project: { "stop.title": 1 } }
+    ],
+    check: [
+      { "_id" : 1, "stop" : { "title" : "book1" } },
+      { "_id" : 2, "stop" : [ { "title" : "book2" }, { "title" : "book3" } ] }
+    ]
+  },
+  {
+    message: "project include specific fields from embedded documents using nested array syntax",
+    input: [
+      { _id: 1, user: "1234", stop: { title: "book1", author: "xyz", page: 32 } },
+      { _id: 2, user: "7890", stop: [ { title: "book2", author: "abc", page: 5 }, { title: "book3", author: "ijk", page: 100 } ] }
+    ],
+    query: [ { $project: { stop: { title: 1 } } } ],
+    check: [
+      { "_id" : 1, "stop" : { "title" : "book1" } },
+      { "_id" : 2, "stop" : [ { "title" : "book2" }, { "title" : "book3" } ] }
+    ]
+  },
+  {
+    message: "project including computed fields",
+    input: [
+      {
+        "_id" : 1,
+        title: "abc123",
+        isbn: "0001122223334",
+        author: { last: "zzz", first: "aaa" },
+        copies: 5
+      }
+    ],
+    query: [
+      {
+        $project: {
+           title: 1,
+           isbn: {
+              prefix: { $substr: [ "$isbn", 0, 3 ] },
+              group: { $substr: [ "$isbn", 3, 2 ] },
+              publisher: { $substr: [ "$isbn", 5, 4 ] },
+              title: { $substr: [ "$isbn", 9, 3 ] },
+              checkDigit: { $substr: [ "$isbn", 12, 1] }
+           },
+           lastName: "$author.last",
+           copiesSold: "$copies"
+        }
+      }
+    ],
+    check: [
+      {
+        "_id" : 1,
+        "title" : "abc123",
+        "isbn" : {
+           "prefix" : "000",
+           "group" : "11",
+           "publisher" : "2222",
+           "title" : "333",
+           "checkDigit" : "4"
+        },
+        "lastName" : "zzz",
+        "copiesSold" : 5
+      }
+    ]
+  },
+  {
+    message: "project new array fields",
+    input: [ { "_id" : 1, "x" : 1, "y" : 1 } ],
+    query: [ { $project: { myArray: [ "$x", "$y" ] } } ],
+    check: [ { "_id" : 1, "myArray" : [ 1, 1 ] } ]
+  },
+  {
+    message: "project new array fields with mssing fields",
+    input: [ { "_id" : 1, "x" : 1, "y" : 1 } ],
+    query: [ { $project: { myArray: [ "$x", "$y", "$someField" ] } } ],
+    check: [ { "_id" : 1, "myArray" : [ 1, 1, null ] } ]
+  },
+  // test from https://github.com/kofrasa/mingo/issues/119
+  {
+    message: 'should project new array fields (see #119)',
+    input: [
+      { event: { x: "hi" } }
+    ],
+    query: [
+      {
+        $project: {
+          myArray: ["$event.x"]
+        }
+      }
+    ],
+    check: [
+      { myArray: ['hi'] }
+    ]
   }
-
 ])
