25e8c3fb6c9eeba282651923f1eda9fb89943582
Francis Asante <kofrasa@gmail.com>
2020-04-04 00:08:23 +0200

    Fix test runner to execute on travis-ci.
    - Add typescript to devDependencies
    - Install typescript and esm in travis. Remove esm from dev dependencies
    - Use wrapper for loading 'esm' for testing on travis
    - Cleanup unused renovate dependencies

---
+++ runtest.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# declare the tests to run. all tests by default
+TESTS=${1:-"test/*.js test/**/*.js"}
+
+# define the temporary main test script
+FILE=testmain.js
+
+# register cleanup
+trap "rm -f ${FILE}" EXIT
+
+# create test script
+> $FILE cat <<-eof
+import test from 'tape'
+import path  from 'path'
+
+test.createStream().pipe(process.stdout)
+
+process.argv.slice(2).forEach(function (file) {
+  require(path.resolve(file))
+})
+eof
+
+# execute test with esm
+node -r esm ${FILE} ${TESTS}
