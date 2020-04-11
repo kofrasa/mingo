0179906aa05ffcc3d643f89831d3cbd8f740d804
Francis Asante <kofrasa@gmail.com>
2020-04-11 02:41:20 +0200

    Use ES5 syntax for `prepare.js` and improve packaging scripts
    - Use node implementations for file operations
    - Handle tarball if generated
    - Do not execute prepare script with `esm`

--- prepare.js
+++ prepare.js
@@ -1,23 +1,35 @@
-#!/usr/bin/env node -r esm
+let fs = require('fs')
+let path = require('path')
+let execSync = require('child_process').execSync
+let packageJson = require('./package.json')
 
-import fs  from 'fs'
-import path from 'path'
-import { execSync } from 'child_process'
-import packageJson from './package.json'
+const LIB_DIR = path.resolve('lib')
 
-const LIB_DR = path.resolve('lib')
 const CMD = process.argv.slice(2).join(' ')
-const MAIN_JS_FILE = 'main.js'
-const MAIN_JS_DATA = `
-require = require("esm")(module/*, options*/)
-module.exports = require("./index.js")
-`
 
-// paths to exclude
-const EXCLUDE = [
+const MAIN_JS_FILE = 'main.js'
+const MAIN_JS_DATA = [
+  'require = require("esm")(module/*, options*/)',
+  'module.exports = require("./index.js")'
+].join('\n')
+
+// files to cleanup to avoid packing
+const CLEANUP_FILES = [
   'node_modules',
   'package-lock.json'
 ]
+
+// .npmignore
+const NPM_IGNORE = [
+  '.*',
+  '*.tgz',
+  'node_modules',
+  'package-lock.json'
+]
+
+function cleanup() {
+  CLEANUP_FILES.forEach(f => fs.rmdirSync(path.join(LIB_DIR, f), { recursive: true }))
+}
 
 /**
  * Prepares the lib directory for distributting
@@ -25,15 +37,22 @@ const EXCLUDE = [
 function prepare() {
 
   // ensure directory exists
-  execSync(`mkdir -p ${LIB_DR}`)
+  if (!fs.existsSync(LIB_DIR)) {
+    console.error("Directory", LIB_DIR, "does not exist")
+    process.exit(1)
+  }
 
-  // delete any node_modules file
-  EXCLUDE.forEach(p => execSync(`rm -fr ${path.join(LIB_DR, p)}`))
+  console.log("Preparing", LIB_DIR)
+
+  cleanup()
+
+  // write ignore file
+  fs.writeFileSync(path.join(LIB_DIR, '.npmignore'), NPM_IGNORE.join('\n'))
 
   // copy all the allowed files to the lib directory
   packageJson.files = packageJson.files.reduce((files, p) => {
     if (!p.match(/(lib|index\.js)/)) {
-      fs.copyFileSync(path.resolve(p), path.resolve(path.join(LIB_DR, p)))
+      fs.copyFileSync(path.resolve(p), path.join(LIB_DIR, p))
       files.push(p)
     }
     return files
@@ -43,7 +62,7 @@ function prepare() {
   ])
 
   // write main entry files using esm
-  fs.writeFileSync(path.resolve(path.join(LIB_DR, MAIN_JS_FILE)), MAIN_JS_DATA)
+  fs.writeFileSync(path.join(LIB_DIR, MAIN_JS_FILE), MAIN_JS_DATA)
 
   // override entry files
   packageJson.main = MAIN_JS_FILE
@@ -52,31 +71,40 @@ function prepare() {
 
   // clear all scripts
   packageJson.scripts = {}
+  packageJson.devDependencies = {}
 
   let data = JSON.stringify(packageJson, null, 2)
 
   // write new package.json for lib
-  fs.writeFileSync(path.join(LIB_DR, 'package.json'), data)
+  fs.writeFileSync(path.join(LIB_DIR, 'package.json'), data)
 
-  // print the resulting package.json
-  console.log(data)
+  console.log("Prepared", LIB_DIR)
 }
 
 function main() {
-  console.log(`Preparing package subfolder ${LIB_DR}\n`)
   prepare()
 
-  // if a command was supplied execute it within the lib folder
   if (CMD) {
-    let npm_cmd = `npm ${CMD}`
+    // execute within lib dir
+    let npm_cmd = "npm " + CMD
 
-    console.log(`\nExecuting command: ${npm_cmd}\n`)
+    console.log("\nExecuting command:", npm_cmd, "\n")
 
-    process.chdir(LIB_DR)
+    // execute command
+    execSync(npm_cmd, { cwd: LIB_DIR, env: process.env, stdio: 'inherit' })
 
-    console.log(execSync(npm_cmd)+"\n")
     console.log("\nCompleted command\n")
+
+    // if we created a tar file, copy to parent directory
+    let tarball = packageJson.name + '-' + packageJson.version + '.tgz'
+    let tarballPath = path.join(LIB_DIR, tarball)
+    if (fs.existsSync(tarballPath)) {
+      console.log("Copying", tarball, "to correct folder")
+      fs.renameSync(tarballPath, path.join(path.dirname(LIB_DIR), tarball))
+    }
   }
+
+  cleanup()
 }
 
 main()
