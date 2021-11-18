23aeb487620ac86f5375f77f7a4593e3571ebaf9
Francis Asante <kofrasa@gmail.com>
2021-11-17 19:52:30 -0800

    test: Migrate expression/arithmetic tests to jest

--- test/support.ts
+++ test/support.ts
@@ -1,11 +1,8 @@
 import "../src/init/system";
 
-import tape from "tape";
-
 import { aggregate } from "../src";
 import { computeValue } from "../src/core";
 import { RawArray, RawObject } from "../src/types";
-import { isObjectLike } from "../src/util";
 import complexGrades from "./data/grades_complex";
 import simpleGrades from "./data/grades_simple";
 import person from "./data/person";
@@ -164,55 +161,52 @@ export function runTest(
   Object.entries(suite).forEach((arr) => {
     const operator = arr[0];
     const examples = arr[1] as Array<Array<unknown>>;
-    tape(description + ": " + operator, (t) => {
-      examples.forEach((val) => {
-        let input = val[0];
-        let expected = val[1];
-        const ctx = (val[2] || { err: false }) as RawObject;
-        const obj = ctx?.obj || {};
+    describe(description, () => {
+      describe(operator, () => {
+        examples.forEach((val) => {
+          let input = val[0];
+          let expected = val[1];
+          const ctx = (val[2] || { err: false }) as RawObject;
+          const obj = ctx?.obj || {};
 
-        let field = operator;
-        // use the operator as field if not present in input
-        if (!!input && input.constructor === Object) {
-          field = Object.keys(input).find((s) => s[0] === "$") || null;
-          if (field === null) {
-            field = operator;
-          } else {
-            input = input[field];
+          let field = operator;
+          // use the operator as field if not present in input
+          if (!!input && input.constructor === Object) {
+            field = Object.keys(input).find((s) => s[0] === "$") || null;
+            if (field === null) {
+              field = operator;
+            } else {
+              input = input[field];
+            }
           }
-        }
 
-        if (ctx.err) {
-          t.throws(
-            () => computeValue(obj, input, field),
-            `${operator}(${
-              isObjectLike(input) ? JSON.stringify(input) : input
-            }) => Error("${expected}")`
-          );
-        } else {
-          let actual = computeValue(obj, input, field);
-          // NaNs don't compare
-          if (actual !== actual && expected !== expected) actual = expected = 0;
-          t.deepEqual(
-            actual,
-            expected,
-            `${operator}(${
-              isObjectLike(input) ? JSON.stringify(input) : input
-            }) => ` +
-              `${isObjectLike(expected) ? JSON.stringify(expected) : expected}`
-          );
-        }
+          const prefix = `can apply ${operator}(${JSON.stringify(input)})`;
+
+          if (ctx.err) {
+            it(`${prefix} => Error("${expected}")`, () => {
+              expect(() => computeValue(obj, input, field)).toThrow(Error);
+            });
+          } else {
+            it(`${prefix} => ${JSON.stringify(expected)}`, () => {
+              let actual = computeValue(obj, input, field);
+              // NaNs don't compare
+              if (actual !== actual && expected !== expected) {
+                actual = expected = 0;
+              }
+              expect(actual).toEqual(expected);
+            });
+          }
+        });
       });
-      t.end();
     });
   });
 }
 
 interface PipelineTestSuite {
   input: RawArray;
-  query: Array<RawObject>;
-  check: RawArray | ((result: RawArray, test: tape.Test) => void);
-  message?: string;
+  pipeline: Array<RawObject>;
+  expected: RawArray;
+  message: string;
 }
 /**
  * run pipeline test
@@ -221,19 +215,11 @@ export function runTestPipeline(
   description: string,
   suite: Array<PipelineTestSuite>
 ): void {
-  tape(description, (t) => {
+  describe(description, () => {
     suite.forEach((unitTest) => {
-      const input = unitTest.input;
-      const pipeline = unitTest.query;
-      const check = unitTest.check;
-      const message = unitTest.message || "actual equals expected";
+      const { input, pipeline, expected, message } = unitTest;
       const actual = aggregate(input, pipeline);
-      if (typeof check === "function") {
-        check(actual, t);
-      } else {
-        t.deepEqual(actual, check, message);
-      }
+      it(message, () => expect(actual).toEqual(expected));
     });
-    t.end();
   });
 }
